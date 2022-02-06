356106ec82763dc051011e9d6f26bc1d805f1890
Omar Aguirre <omar.fi.wwr@gmail.com>
2022-02-05 21:29:04 -0600

    Push to Heroku

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -137,3 +137,19 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# HEROKU CONFIG
+# Update database configuration with $DATABASE_URL.
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- video_downloader/wsgi.py
+++ video_downloader/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'video_downloader.settings')
 
 application = get_wsgi_application()
+
+# HEROKU CONFIG
+from whitenoise.django import DjangoWhiteNoise  
+application = DjangoWhiteNoise(application)
