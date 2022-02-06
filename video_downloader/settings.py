b06c8a3bf38ba21a1961b1ed62b78789c9e5d66a
Omar Aguirre <omar.fi.wwr@gmail.com>
2022-02-05 21:55:41 -0600

    Correción del WhiteNoise

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -59,7 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'video_downloader.urls'
